7698a7e4be4bbe4ac3912153a81e7b8b665f963c
wulei123 <1534563365@qq.com>
2016-11-07 07:27:24 +0800

    接入验证完成

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 [![Build Status](https://travis-ci.org/wulei123/wechat.svg?branch=master)](https://travis-ci.org/wulei123/wechat)
 
 这是一个laravel的微信SDK扩展包
+##接入微信公众平台
+>在.env中添加WECHAT_TOKEN=yourtoken
 
 很抱歉，这个项目还没有完成。。。。
--- src/Access.php
+++ src/Access.php
@@ -1,9 +1,24 @@
 <?php
 
 namespace WeChat;
+use Illuminate\Support\Facades\Input;
+
 class Access
 {
-    public function access($arguments){
-        return $arguments[0];
+    public function access(){
+        $signature = Input::get("signature");
+        $timestamp = Input::get("timestamp");
+        $nonce = Input::get("nonce");
+        $echostr = Input::get("echostr");
+        $token = env('WECHAT_TOKEN');//替换为在公众平台填写的token
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);//将数组中的字符串按字典序排序
+        $tmpStr = implode( $tmpArr );//将所有字符串拼接起来
+        $tmpStr = sha1( $tmpStr );//用sha1加密
+        if( $tmpStr == $signature ){
+            echo $echostr;//必须echo传来的echostr参数作为响应
+        }else{
+            echo false;
+        }
     }
 }
